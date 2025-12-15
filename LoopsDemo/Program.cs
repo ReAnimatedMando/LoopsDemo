f346eeee36c532c0b88ddaf2337733dd4380798c
Aaron Tanguma <the_guma@yahoo.com>
2025-12-15 08:59:52 -0700

    created project folder, added while loop example

---
+++ LoopsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LoopsDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // while loop
+        Console.WriteLine("while loop");
+
+        // var cont = "yes";
+        //
+        // while (cont == "yes")
+        // {
+        //     Console.WriteLine("Continue? (type 'yes' or 'no')");
+        //     cont = Console.ReadLine();
+        // }
+
+        // var number = 0;
+        //
+        // while (number <= 5)
+        // {
+        //     Console.WriteLine(number);
+        //     number++;
+        // }
+        // a while loop may never run because the condition may never even be met. asks the question first
+        
+        // do-while loop
+        Console.WriteLine("do-while loop");
+        
+        
+    }
+}
